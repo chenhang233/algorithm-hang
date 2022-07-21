a81a42ab6d4b115f4fc6e7c133a9b3787d2d2de7
chenhang <chenhang223333@163.com>
2022-07-21 15:17:19 +0800

    10好难

---
+++ LeetCode/09. 回文数.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+  const arr = x.toString().split('')
+  for (let i = 0, j = arr.length - 1; i < j; ++i, --j) {
+    if (arr[i] !== arr[j]) {
+      return false
+    }
+  }
+  return true
+}
