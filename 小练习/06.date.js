7a3475fef91644ae835ed513e9856ce743edca8f
chenhang <chenhang223333@163.com>
2022-07-27 17:11:44 +0800

    test

--- 小练习/06.date.js
+++ 小练习/06.date.js
@@ -1,3 +1,10 @@
-const a = new Date()
-
-console.log(a.toLocaleDateString().replace(/\//g, ''))
+const a = new Date('2022-1-1')
+// replace(/\//g,)
+let k = a
+  .toLocaleDateString('zh-CN', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  })
+  .replace(/\//g, '')
+console.log(k)
