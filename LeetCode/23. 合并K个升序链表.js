4353ecea9f9fbcd0ea9f7e3194673bb274b2f1c0
chenhang <chenhang223333@163.com>
2022-08-09 08:59:41 +0800

    LeetCode:23-2

--- LeetCode/23. 合并K个升序链表.js
+++ LeetCode/23. 合并K个升序链表.js
@@ -40,3 +40,43 @@ var mergeKLists = function (lists) {
   arrList.forEach((v) => (myList = myPush(v, myList)))
   return myList
 }
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists2 = function (lists) {
+  const len = lists.length
+  if (lists.length === 0) return null
+  if (lists.length === 1) return lists[0]
+  let myList = null
+  for (let i = 0; i < len; ++i) {
+    myList = mergeList(myList, lists[i])
+  }
+  function mergeList(L1, L2) {
+    const node = new ListNode()
+    let current = node
+    while (L1 && L2) {
+      if (L1.val < L2.val) {
+        current.next = L1
+        L1 = L1.next
+      } else {
+        current.next = L2
+        L2 = L2.next
+      }
+      current = current.next
+    }
+    if (L1) current.next = L1
+    if (L2) current.next = L2
+    return node.next
+  }
+  console.log(myList, 'myList')
+  return myList
+}
