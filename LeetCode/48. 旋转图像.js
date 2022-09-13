f8a984437127e85d7bf35949369c75558cc38b46
chenhang <chenhang223333@163.com>
2022-09-13 10:02:15 +0800

    LeetCode:48

--- LeetCode/48. 旋转图像.js
+++ LeetCode/48. 旋转图像.js
@@ -16,3 +16,20 @@ var rotate = function (matrix) {
     }
   }
 }
+
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate2 = function (matrix) {
+  const n = matrix.length
+  for (let i = 0; i < Math.floor(n / 2); i++) {
+    for (let j = 0; j < Math.floor((n + 1) / 2); j++) {
+      let temp = matrix[i][j]
+      matrix[i][j] = matrix[n - 1 - j][i]
+      matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
+      matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
+      matrix[j][n - 1 - i] = temp
+    }
+  }
+}
