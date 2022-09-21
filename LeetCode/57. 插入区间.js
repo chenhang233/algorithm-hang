83678c5e41be42029e0937046a7e0719a934ea61
chenhang <chenhang223333@163.com>
2022-09-21 09:44:15 +0800

    LeetCode: 57 js .

--- LeetCode/57. 插入区间.js
+++ LeetCode/57. 插入区间.js
@@ -21,3 +21,29 @@ var insert = function (intervals, newInterval) {
   res.push([begin, end])
   return res
 }
+
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+var insert2 = function (intervals, newInterval) {
+  const res = []
+  const n = intervals.length
+  let r = 0
+  while (r < n && intervals[r][1] < newInterval[0]) {
+    res.push(intervals[r])
+    r++
+  }
+  while (r < n && intervals[r][0] <= newInterval[1]) {
+    newInterval[0] = Math.min(newInterval[0], intervals[r][0])
+    newInterval[1] = Math.max(newInterval[1], intervals[r][1])
+    r++
+  }
+  res.push(newInterval)
+  while (r < n) {
+    res.push(intervals[r])
+    r++
+  }
+  return res
+}
