f9c626afa0d7eb2c7d5a82127e5ec1d06f18320c
chenhang <chenhang223333@163.com>
2022-09-29 14:42:52 +0800

    exercise: 动态规划练习 01

---
+++ 11.算法设计与技巧/02.动态规划/exercise/02.背包.js
@@ -0,0 +1,7 @@
+const knapSack = (capacity, weights, values, n) => {}
+
+const values = [3, 4, 5],
+  weights = [2, 3, 4],
+  capacity = 5,
+  n = values.length
+knapSack(capacity, weights, values, n)
