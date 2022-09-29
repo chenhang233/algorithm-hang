e6dab05ac5cf322b56fbddd2e543000e7ab3e7ee
chenhang <chenhang223333@163.com>
2022-09-29 17:29:24 +0800

    exercise: 动态规划 02

--- 11.算法设计与技巧/02.动态规划/exercise/02.背包.js
+++ 11.算法设计与技巧/02.动态规划/exercise/02.背包.js
@@ -1,7 +1,41 @@
-const knapSack = (capacity, weights, values, n) => {}
+const findValues = (n, capacity, db, weights, values) => {
+  let i = n
+  let k = capacity
+  while (i > 0 && k > 0) {
+    if (db[i][k] !== db[i - 1][k]) {
+      console.log(`物品${i}的重量是${weights[i - 1]}, 价值是${values[i - 1]}`)
+      i--
+      k -= db[i][k]
+    } else {
+      i--
+    }
+  }
+}
 
-const values = [3, 4, 5],
-  weights = [2, 3, 4],
-  capacity = 5,
-  n = values.length
-knapSack(capacity, weights, values, n)
+const knapSack = (capacity, weights, values, n) => {
+  const db = []
+  for (let i = 0; i <= n; i++) {
+    db[i] = []
+  }
+  for (let i = 0; i <= n; i++) {
+    for (let j = 0; j <= capacity; j++) {
+      if (i === 0 || j === 0) {
+        db[i][j] = 0
+      } else if (weights[i - 1] <= j) {
+        const a = values[i - 1] + db[i - 1][j - weights[i - 1]]
+        const b = db[i - 1][j]
+        db[i][j] = a > b ? a : b
+      } else {
+        db[i][j] = db[i - 1][j]
+      }
+    }
+  }
+  findValues(n, capacity, db, weights, values)
+  return db[n][capacity]
+}
+
+const values = [3, 4, 5], // 包值
+  weights = [2, 3, 4], // 包重量
+  capacity = 5, // 容量
+  n = values.length // 包数量
+console.log(knapSack(capacity, weights, values, n))
