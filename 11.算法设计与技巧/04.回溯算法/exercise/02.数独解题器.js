ce1bedc415b0db73da8d61cb0c2b6f9289eee11b
陈杭 <1973433180@qq.com>
2022-11-01 10:57:02 +0800

    回溯算法: exercise 02 数独 js

--- 11.算法设计与技巧/04.回溯算法/exercise/02.数独解题器.js
+++ 11.算法设计与技巧/04.回溯算法/exercise/02.数独解题器.js
@@ -1,5 +1,46 @@
-const isSafe = () => {}
-const sudokuSolver = (board) => {}
+const isSafe = (board, row, col, n) => {
+  let flag = true
+  for (let i = 0; i < col; i++) {
+    if (board[row][i] === n) flag = false
+  }
+  for (let i = 0; i < board.length; i++) {
+    if (board[i][col] === n) flag = false
+  }
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (board[i + (row - (row % 3))][j + (col - (col % 3))] === n)
+        flag = false
+    }
+  }
+  return flag
+}
+const sudokuSolver = (board) => {
+  const y = board.length
+  const x = board[0].length
+  let i = 0,
+    j = 0
+  let flag = false
+  for (i = 0; i < y; i++) {
+    for (j = 0; j < x; j++) {
+      if (board[i][j] === 0) {
+        flag = true
+        break
+      }
+    }
+    if (flag) break
+  }
+  if (!flag) return true
+  for (let n = 1; n <= 9; n++) {
+    if (isSafe(board, i, j, n)) {
+      board[i][j] = n
+      if (sudokuSolver(board)) {
+        return true
+      }
+      board[i][j] = 0
+    }
+  }
+  return false
+}
 
 const sudokuGrid = [
   [5, 3, 0, 0, 7, 0, 0, 0, 0],
@@ -13,3 +54,4 @@ const sudokuGrid = [
   [0, 0, 0, 0, 8, 0, 0, 7, 9],
 ]
 console.log(sudokuSolver(sudokuGrid))
+console.log(sudokuGrid)
