6521971d4b1a93eca221d3aee33f44f24d0452f2
chenhang233 <1973433180@qq.com>
2022-11-25 18:47:59 +0800

    LeetCode: 809 js

--- LeetCode/809. 情感丰富的文字.js
+++ LeetCode/809. 情感丰富的文字.js
@@ -6,17 +6,25 @@
 var expressiveWords = function (s, words) {
   let count = 0
   const expend = (s, word) => {
-    let current_s = s[0]
-    let current_w = word[0]
-    if (current_s !== current_w) return
-    let s_i = 1
-    let s_count = 0
-    while (s[s_i] === current_s) {
-      s_i++
-      s_count++
+    let i = 0,
+      j = 0
+    while (i < s.length && j < word.length) {
+      if (s[i] !== word[j]) return
+      const ss = s[i]
+      let s_c = 0
+      while (i < s.length && s[i] === ss) {
+        s_c++
+        i++
+      }
+      let w_c = 0
+      while (j < word.length && word[j] === ss) {
+        j++
+        w_c++
+      }
+      if (w_c > s_c) return
+      if (w_c !== s_c && s_c < 3) return
     }
-    let w_i = 1
-    let w_count = 0
+    return s.length === i && word.length === j
   }
   for (const word of words) {
     if (expend(s, word)) count++
