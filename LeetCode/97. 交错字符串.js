0885936dadbf5afdbb6edaed8022537a49bfdf45
陈杭 <1973433180@qq.com>
2022-12-14 16:33:08 +0800

    LeetCode: 97 doing

--- LeetCode/97. 交错字符串.js
+++ LeetCode/97. 交错字符串.js
@@ -4,22 +4,24 @@
  * @param {string} s3
  * @return {boolean}
  */
-var isInterleave = function (s1, s2, s3) {
-  const t1 = s1 + s2
-  const map = {}
-  for (let i = 0; i < t1.length; i++) {
-    map[t1[i]] = (map[t1[i]] || 0) + 1
-  }
-  console.log(map)
-  for (let j = 0; j < s3.length; j++) {
-    console.log(s3[j], 's3[j]')
-    if (map[s3[j]]) {
-      map[s3[j]]--
-    } else {
-      return false
-    }
-  }
-  return true
-}
+// const isInterleave = function (s1, s2, s3) {
+//   const cache = {}
+//   const _ = (s1, s2, s3) => {
+//     const cacheKey = s1 + ',' + s2 + ',' + s3
+//     if (cache[cacheKey] !== undefined) return cache[cacheKey]
+//     if (s1.length + s2.length !== s3.length) return (cache[cacheKey] = false)
+//     if (!s3) return (cache[cacheKey] = !s1 && !s2)
+//     if (s3[0] !== s1[0] || s3[0] !== s2[0]) {
+//       return s3[0] === s1[0]
+//         ? (cache[cacheKey] = _(s1.slice(1), s2, s3.slice(1)))
+//         : s3[0] === s2[0]
+//         ? (cache[cacheKey] = _(s1, s2.slice(1), s3.slice(1)))
+//         : false
+//     }
+//     return (cache[cacheKey] =
+//       _(s1.slice(1), s2, s3.slice(1)) || _(s1, s2.slice(1), s3.slice(1)))
+//   }
+//   return _(s1, s2, s3)
+// }
 
-isInterleave('aabcc', 'dbbca', 'aadbbbaccc')
+// isInterleave('aabcc', 'dbbca', 'aadbbbaccc')
