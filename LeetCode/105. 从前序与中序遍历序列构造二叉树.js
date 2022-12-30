fd6fdb4d56022ac13c54ce244bf1d9c381637191
chenhang233 <1973433180@qq.com>
2022-12-30 13:15:53 +0800

    LeetCode: 105 js todo

---
+++ LeetCode/105. 从前序与中序遍历序列构造二叉树.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function (preorder, inorder) {
+  if (!preorder.length) return null
+  const rootV = preorder[0]
+  const root = new TreeNode(rootV)
+  const mid = inorder.findIndex((v) => v === rootV)
+  root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid))
+  root.right = buildTree(preorder.slice(mid + 1), inorder.slice(mid + 1))
+  return root
+}
