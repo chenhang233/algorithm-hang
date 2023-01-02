a9961bca51a8fbded28eaedf128b5249a4267950
chenhang233 <1973433180@qq.com>
2023-01-02 12:46:39 +0800

    LeetCode: 2521 js

--- LeetCode/2521. 数组乘积中的不同质因数数目.js
+++ LeetCode/2521. 数组乘积中的不同质因数数目.js
@@ -1,35 +1,6 @@
 /**
  * @param {number[]} nums
  * @return {number}
- * 超时
- */
-var distinctPrimeFactors = function (nums) {
-  const reduce = nums.reduce((a, v) => (a *= v), 1)
-  let s = ''
-  let count = 0
-  const map = {}
-  const primeNum = (n, m, result) => {
-    console.log(n, m, result)
-    if (n < m) s = result
-    else {
-      if (n % m == 0) {
-        primeNum(n / m, m, result + m + ' ')
-      } else primeNum(n, m + 1, result)
-    }
-  }
-  primeNum(reduce, 2, '')
-  s.split(' ').forEach((s) => {
-    if (s && !map[s]) {
-      map[s] = true
-      count++
-    }
-  })
-  return count
-}
-
-/**
- * @param {number[]} nums
- * @return {number}
  */
 var distinctPrimeFactors2 = function (nums) {
   const search = (n) => {
