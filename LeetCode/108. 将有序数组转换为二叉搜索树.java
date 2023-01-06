e08f783d3da0a86255fbc5e51e9816952967211d
chenhang233 <1973433180@qq.com>
2023-01-06 12:55:37 +0800

    LeetCode: 109 js

--- LeetCode/108. 将有序数组转换为二叉搜索树.java
+++ LeetCode/108. 将有序数组转换为二叉搜索树.java
@@ -1,19 +1,28 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public TreeNode sortedArrayToBST(int[] nums) {
+    /**
+    * Definition for a binary tree node.
+    * public class TreeNode {
+    *     int val;
+    *     TreeNode left;
+    *     TreeNode right;
+    *     TreeNode() {}
+    *     TreeNode(int val) { this.val = val; }
+    *     TreeNode(int val, TreeNode left, TreeNode right) {
+    *         this.val = val;
+    *         this.left = left;
+    *         this.right = right;
+    *     }
+    * }
+    */
+    class Solution {
+        public TreeNode sortedArrayToBST(int[] nums) {
+         return   help(nums, 0, nums.length -1);
+        }
+         public  TreeNode help(int[] nums, int left, int right) {
+            if (left > right) return null;
+            int index = (left + right) / 2;
+            TreeNode node = new TreeNode(nums[index]);
+            node.left = help(nums, left, index - 1);
+            node.right = help(nums, index + 1,right);
+            return  node;
     }
-}
+    }
--- LeetCode/109. 有序链表转换二叉搜索树.js
+++ LeetCode/109. 有序链表转换二叉搜索树.js
@@ -33,3 +33,40 @@ var sortedListToBST = function (head) {
   }
   return getNode(queue)
 }
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {TreeNode}
+ */
+var sortedListToBST2 = function (head) {
+  const queue = []
+  while (head) {
+    const node = new TreeNode(head.val)
+    queue.push(node)
+    head = head.next
+  }
+  const g = (queue, left, right) => {
+    if (left > right) return null
+    const mid = Math.floor((left + right) / 2)
+    queue[mid].left = g(queue, left, mid - 1)
+    queue[mid].right = g(queue, mid + 1, right)
+    return queue[mid]
+  }
+
+  return g(queue, 0, queue.length - 1)
+}
