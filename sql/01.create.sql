c36230a417a14ad69048960447b9b757baf42cdf
chenhang233 <1973433180@qq.com>
2023-02-06 21:23:38 +0800

    sql: 练习

---
+++ sql/01.create.sql
@@ -0,0 +1,10 @@
+create table student (
+    id int,
+    name varchar(10),
+    gender char(1),
+    birthday date,
+    score double(5,2),
+    email varchar(64),
+    tel varchar(15),
+    status tinyint
+);
