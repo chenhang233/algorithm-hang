8ac79779a181126aec9ea42f5d0f2959aed475d3
陈杭 <1973433180@qq.com>
2023-03-17 16:56:17 +0800

    LeetCode: 1264 sql

---
+++ LeetCode/1264. 页面推荐.sql
@@ -0,0 +1,16 @@
+
+# Write your MySQL query statement below
+SELECT DISTINCT page_id recommended_page 
+FROM Likes 
+WHERE user_id IN (
+    SELECT 
+    user2_id user_id 
+    FROM Friendship WHERE user1_id = 1
+    UNION 
+    SELECT
+    user1_id user_id
+    FROM Friendship WHERE user2_id = 1
+) AND page_id
+ NOT IN (
+     SELECT page_id FROM Likes WHERE user_id = 1
+);
