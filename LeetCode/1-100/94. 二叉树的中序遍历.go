32a49863d417176607fae15db8c82e432ace7166
陈杭 <1973433180@qq.com>
2023-03-20 09:04:59 +0800

    LeetCode: 94 go

---
+++ LeetCode/1-100/94. 二叉树的中序遍历.go
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+ func inorderTraversal(root *TreeNode) (res []int)  {
+	node := root
+	var stack []*TreeNode
+	for node != nil || len(stack) > 0 {
+		for node != nil {
+			stack = append(stack, node)
+			node = node.Left
+		}
+		node = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		res = append(res, node.Val)
+		node = node.Right
+	}
+	return
+}
