95eaa3163c01dcf4a5fa01fd4cd0a0efbf8b6cde
陈杭 <1973433180@qq.com>
2023-03-28 15:39:39 +0800

    LeetCode: 35 java

--- LeetCode/1-100/35. 搜索插入位置.java
+++ LeetCode/1-100/35. 搜索插入位置.java
@@ -6,4 +6,10 @@ class Solution {
         }
         return n;
     }
+        public int searchInsert(int[] nums, int target) {
+    for (int i = 0; i < nums.length; i++) {
+                if (nums[i] >= target) return  i;
+        }
+        return nums.length;
+    }
 }
