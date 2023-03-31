25344bc3ecf9699d2d01c8ed4381be367adaa6c6
chenhang233 <1973433180@qq.com>
2023-03-31 14:23:30 +0800

    LeetCode: 279 java

---
+++ LeetCode/201-300/279.完全平方数.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numSquares(int n) {
+         int[] f = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++) {
+                min = Math.min(min,f[i - j * j]);
+            }
+            f[i] = min + 1;
+        }
+        return f[n];
+    }
+}
