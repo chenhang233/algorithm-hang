40e88f69272e6c936abe516ce4f944582484b085
chenhang233 <1973433180@qq.com>
2023-04-30 11:26:44 +0800

    LeetCode: 101 2  go

--- LeetCode/101-200/101. 对称二叉树.go
+++ LeetCode/101-200/101. 对称二叉树.go
@@ -21,3 +21,30 @@ func diff(l, r *TreeNode) bool {
 		return diff(l.Left, r.Right) && diff(l.Right, r.Left)
 	}
 }
+
+
+
+ func isSymmetric(root *TreeNode) bool {
+	var stack []*TreeNode
+	l, r := root, root
+	stack = append(stack, l)
+	stack = append(stack, r)
+	for len(stack) > 0 {
+		l, r = stack[0], stack[1]
+		stack = stack[2:]
+		if l == nil && r == nil {
+			continue
+		}
+		if l == nil || r == nil {
+			return false
+		}
+		if l.Val != r.Val {
+			return false
+		}
+		stack = append(stack, l.Left)
+		stack = append(stack, r.Right)
+		stack = append(stack, l.Right)
+		stack = append(stack, r.Left)
+	}
+	return true
+}
