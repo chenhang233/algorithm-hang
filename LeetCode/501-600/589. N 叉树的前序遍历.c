fb65648535f34216e1abbd17c2e0cce59cd9c36e
chenhang233 <1973433180@qq.com>
2023-05-20 17:46:07 +0800

    LeetCode: 589 c

--- LeetCode/501-600/589. N 叉树的前序遍历.c
+++ LeetCode/501-600/589. N 叉树的前序遍历.c
@@ -35,3 +35,40 @@ int *preorder(struct Node *root, int *returnSize)
 }
 
 
+
+/**
+ * Definition for a Node.
+ * struct Node {
+ *     int val;
+ *     int numChildren;
+ *     struct Node** children;
+ * };
+ */
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+#define MAX_SIZE 10000
+int *preorder(struct Node *root, int *returnSize)
+{
+    *returnSize = 0;
+    if (root == NULL)
+        return NULL;
+    int *res = malloc(sizeof(int) * MAX_SIZE);
+    struct Node **stack = malloc(sizeof(struct Node *) * MAX_SIZE);
+    int p = 0;
+    stack[p] = root;
+    while (p >= 0)
+    {
+        struct Node *cur = stack[p--];
+        res[(*returnSize)++] = cur->val;
+        for (int i = cur->numChildren - 1; i >= 0; i--)
+        {
+            stack[++p] = cur->children[i];
+        }
+    }
+    return res;
+}
+
+
+
