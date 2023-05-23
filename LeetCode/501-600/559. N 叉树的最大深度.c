c5f4b6158a9739ccfa4f4dddedc7bb9fa1bcc4f8
chenhang <1973433180@qq.com>
2023-05-23 09:03:50 +0800

    LeetCode: 559 c 2

--- LeetCode/501-600/559. N 叉树的最大深度.c
+++ LeetCode/501-600/559. N 叉树的最大深度.c
@@ -23,3 +23,36 @@ int maxDepth(struct Node *root)
         return 0;
     return dfs(root, 1);
 }
+
+/**
+ * Definition for a Node.
+ * struct Node {
+ *     int val;
+ *     int numChildren;
+ *     struct Node** children;
+ * };
+ */
+
+#define MAX_NODE_SIZE 10000
+int maxDepth(struct Node *root)
+{
+    if (root == NULL)
+        return 0;
+    struct Node **queue = malloc(sizeof(struct Node *) * MAX_NODE_SIZE);
+    int head = 0, tail = 0, level = 0;
+    queue[tail++] = root;
+    while (head != tail)
+    {
+        int len = tail - head;
+        for (int i = 0; i < len; i++)
+        {
+            struct Node *cur = queue[head++];
+            for (int j = 0; j < cur->numChildren; j++)
+            {
+                queue[tail++] = cur->children[j];
+            }
+        }
+        level++;
+    }
+    return level;
+}
