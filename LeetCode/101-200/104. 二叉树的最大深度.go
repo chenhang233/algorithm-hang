56bfbbf7e1018848e90047d54252f06a589598ef
chenhang233 <1973433180@qq.com>
2023-07-06 09:35:29 +0800

    LeetCode: 104 go 2

--- LeetCode/101-200/104. 二叉树的最大深度.go
+++ LeetCode/101-200/104. 二叉树的最大深度.go
@@ -18,3 +18,32 @@ func dfs(i int,root *TreeNode) int {
 	right := dfs(i + 1,root.Right)
 	return int(math.Max(float64(left), float64(right)))
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+ func maxDepth(root *TreeNode) int {
+	return dfs(0, root)
+}
+
+func dfs(i int,root *TreeNode) int {
+	if root == nil {
+		return i
+	}
+	l := dfs(i+1,root.Left)
+	r := dfs(i+1,root.Right)
+	return Max(l, r)
+}
+
+func Max(a,b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
