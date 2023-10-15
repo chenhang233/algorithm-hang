37bc15a91c2c38e0a2c7cb39c9dfaede2b4be63c
chenhang233 <1973433180@qq.com>
2023-10-15 09:44:53 +0800

    LeetCode: 1137 c++ 2

--- LeetCode/1137. 第 N 个泰波那契数.cpp
+++ LeetCode/1137. 第 N 个泰波那契数.cpp
@@ -13,4 +13,17 @@ public:
         }
         return sum;
     }
+
+        int tribonacci2(int n) {
+        if (n == 0) return 0;
+        if (n <= 2) return 1;
+        int t1 = 0,t2 = 1, t3 = 1, t4;
+        for (int i = 3; i <= n; i++) {
+            t4 = t1+t2+t3;
+            t1 = t2;
+            t2 = t3;
+            t3 = t4;
+        }
+        return t4;
+    }
 };
