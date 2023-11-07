b9f431b89caa96eeea0c251c1252d7137166331d
chenhang233 <1973433180@qq.com>
2023-11-07 20:12:03 +0800

    LeetCode: 221 c++

---
+++ LeetCode/201-300/221. 最大正方形.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int max_side = 0;
+        int row = matrix.size(),col = matrix[0].size();
+        if (row == 0 && col == 0) return max_side;
+        int dp[row][col];
+        memset(dp, 0, sizeof(int) * row * col);
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == '1') {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = min(min(dp[i - 1][j],dp[i][j - 1]),dp[i-1][j-1]) + 1;
+                    }
+                    max_side = max(max_side,dp[i][j]);
+                }
+            }
+        }
+        return max_side*max_side;
+    }
+};
