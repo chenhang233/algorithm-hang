ee66b2fa57ef5dc9e1e6a31995f4a78741dc8df9
chenhang233 <1973433180@qq.com>
2023-12-08 17:14:23 +0800

    LeetCode: 169 2 c++

--- LeetCode/101-200/169. 多数元素.cpp
+++ LeetCode/101-200/169. 多数元素.cpp
@@ -23,3 +23,32 @@ public:
         return 0;
     }
 };
+
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        int n = nums.size();
+        int p = n / 2;
+        cout << p << endl;
+        unordered_map<int, int> m;
+        for (int i = 0; i < n; i++)
+        {
+            m[nums[i]]++;
+            if (m[nums[i]] > p)
+                return nums[i];
+        }
+        return 0;
+    }
+};
+
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        return nums[nums.size() / 2];
+    }
+};
