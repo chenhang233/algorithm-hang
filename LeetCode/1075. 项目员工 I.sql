7265b9d05410d1763b3370f835fb047d7a3e2bc8
chenhang233 <1973433180@qq.com>
2023-12-11 14:41:51 +0800

    LeetCode: 1075 sql

---
+++ LeetCode/1075. 项目员工 I.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT t1.project_id ,ROUND(AVG(t2.experience_years),2) AS 'average_years'  
+FROM Project t1 
+INNER JOIN Employee t2 
+ON t1.employee_id = t2.employee_id
+GROUP BY t1.project_id;
