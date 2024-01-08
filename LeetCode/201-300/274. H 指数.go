b02a691a0dd38976882b00ca46c2aaef15bac461
chenhang233 <1973433180@qq.com>
2024-01-09 07:37:46 +0800

    LeetCode: 274 go

---
+++ LeetCode/201-300/274. H 指数.go
@@ -0,0 +1,10 @@
+
+import "sort"
+func hIndex(citations []int) int {
+    sort.Ints(citations)
+    h := 0 
+    for i := len(citations) - 1; i >= 0 && citations[i] > h; i-- {
+        h++
+    }
+    return h
+}
