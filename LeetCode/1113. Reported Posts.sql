2a3af985b7903a576ecd8052f4fecd811f835d9b
chenhang233 <1973433180@qq.com>
2024-01-08 12:23:11 +0800

    LeetCode: 1113 sql

---
+++ LeetCode/1113. Reported Posts.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT extra 'report_reason',COUNT(DISTINCT post_id) 'report_count'
+FROM Actions WHERE action_date = '2019-07-04' AND 
+action = 'report' AND extra IS NOT NULL
+GROUP BY extra; 
